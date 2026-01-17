9716e42b3845aac4276671cf81c97497ba9c595e
ajk1701 <ajkorlacki@gmail.com>
2026-01-17 12:56:38 -0500

    a

--- problem-sets/korlacki_ps1.py
+++ problem-sets/korlacki_ps1.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 
 pwd = os.getcwd()
 
-dataset = 'hwk1_convergence.csv'
+dataset = "https://raw.githubusercontent.com/akhandelwal8/globaleconomics/refs/heads/main/hwk/hwk1_convergence.csv"
 
-df = pd.read_csv(pwd+'\\'+dataset,sep='\t')
+df = pd.read_csv(dataset,sep='\t')
                 
 gdp60 = df['gdppc1960']
 gdp00 = df['gdppc2000']
@@ -19,9 +19,19 @@ plt.figure()
 plt.hist(g,bins=np.arange(np.min(g),np.max(g)+0.01,0.01))
 plt.title("Average Annual Growth Rate")
 plt.xlabel("Growth Rate")
-plt.show()
 
 # Part 2
-print(f"The percentiles of the growth rates are:\n\t10th: {np.nanpercentile(g,10)} \
-      \n\t25th: {np.nanpercentile(g,25)}\n\t50th: {np.nanpercentile(g,50)}\n\t75th: {np.nanpercentile(g,75)} \
-      \n\t90th: {np.nanpercentile(g,90)}")
+print(f"The percentiles of the growth rates are:\n\t10th: {np.nanpercentile(g,10):.2g} \
+      \n\t25th: {np.nanpercentile(g,25):.2g}\n\t50th: {np.nanpercentile(g,50):.2g}\n\t75th: {np.nanpercentile(g,75):.2g} \
+      \n\t90th: {np.nanpercentile(g,90):.2g}")
+
+# Part 3
+largestctr = df['code'][np.argmax(g)]
+
+print(largestctr)
+
+
+
+
+
+plt.show()
