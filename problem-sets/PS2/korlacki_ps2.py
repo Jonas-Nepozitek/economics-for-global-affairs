cf68c89beed7a75383751e65c8cd915580f4b394
ajk1701 <ajkorlacki@gmail.com>
2026-01-24 13:34:24 -0500

    ps2 v3

--- problem-sets/PS2/korlacki_ps2.py
+++ problem-sets/PS2/korlacki_ps2.py
@@ -2,19 +2,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+from stargazer.stargazer import Stargazer
+import seaborn as sns
 
 
 dataset = "https://raw.githubusercontent.com/akhandelwal8/globaleconomics/refs/heads/main/hwk/hwk2_accounting.csv"
-
 df = pd.read_csv(dataset,sep='\t')
-
 a = 0.3
 
 # 1A, y = Ak**0.3
 
 # 1B
-
-# 1C
 k60 = df['cn1960']/df['pop1960']
 y60 = df['cgdpo1960']/df['pop1960']
 y18 = df['cgdpo2018']/df['pop2018']
@@ -29,15 +28,46 @@ lny18 = np.log(y18)
 lnk18 = np.log(k18)
 lnA18 = np.log(A18)
 
+# 1B
+n = 58.0
+g = np.log(y18/y60)/n
+
+x = lny60
+x = sm.add_constant(x)
+y = g
+model1 = sm.OLS(y,x,missing='drop')
+res1 = model1.fit(cov_type="HC0")
+print(res1.summary(xname=['const','gdppc1960']))
+# 1C
+
 print(f"The mean of ln A in 1960 is: {np.mean(lnA60)}\nThe standard deviation of ln A in 1960 is: {np.std(lnA60)}")
 
-plt.figure()
+# 1D
+x1 = lnA60
+x1 = sm.add_constant(x1)
+y1 = lny60
+mod2 = sm.OLS(y1,x1,missing='drop')
+res2 = mod2.fit(cov_type="HC0")
+print(res2.summary(xname=['const','ln A']))
 
-'''
 
 plt.figure()
-plt.scatter(logy,logA,label='logA')
-plt.scatter(logy,logk,label='logk')
-plt.xlabel("Log(y)")
-plt.legend()
-plt.show()'''
+sns.regplot(x='lnA', y='lny', data=pd.DataFrame({'lnA' : lnA60, 'lny' : lny60}) ) 
+plt.show()
+
+
+# 1E
+USAind = df.index[df['countrycode'] == 'USA']
+
+tfpUSA18 = A18[USAind]
+
+# 1F
+hypoy18 = (k18**a)*tfpUSA18
+hypog = np.log(hypoy18/y60)/n
+
+x3 = lny60
+x3 = sm.add_constant(x3)
+y3 = hypog
+mod3 = sm.OLS(y3,x3,missing='drop')
+res3 = mod3.fit(cov_type="HC0")
+print(res3.summary(xname=['const','gdppc1960']))
